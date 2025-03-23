e5a1b2af31fafcab5f9e34050da5b9174513e61a
Chertenko <kchek816@gmail.com>
2025-03-23 14:46:28 +0300

    add: animation when changing the active button

--- src/components/ButtonSwitch/ButtonSwitch.tsx
+++ src/components/ButtonSwitch/ButtonSwitch.tsx
@@ -1,24 +1,44 @@
-import React, { useState } from "react"
-import './ButtonSwitch.scss'
+import React, { useState } from "react";
+import "./ButtonSwitch.scss";
 
 export interface ICLSwitch {
-  firstTitle: string
-  secondTitle: string
-  onChange: (activeButton: string) => void
+  firstTitle: string;
+  secondTitle: string;
+  onChange: (activeButton: string) => void;
 }
 
-export default function Switch({firstTitle,secondTitle, onChange}:ICLSwitch) {
-  const [active, setActive] = useState(firstTitle)
+export default function Switch({
+  firstTitle,
+  secondTitle,
+  onChange,
+}: ICLSwitch) {
+  const [active, setActive] = useState(firstTitle);
 
   const handleClick = (title: string) => {
     setActive(title);
-    onChange(title)
-  }
+    onChange(title);
+  };
 
   return (
     <div className="switch-btn-container">
-      <button className={`switch-btn ${active === firstTitle ? 'active' : ''}`} onClick={() => handleClick(firstTitle)}>{firstTitle}</button>
-      <button className={`switch-btn ${active === secondTitle ? 'active' : ''}`} onClick={() => handleClick(secondTitle)}>{secondTitle}</button>
+      <div
+        className="highlight"
+        style={{
+          transform: `translateX(${active === firstTitle ? 0 : 124}px)`,
+        }}
+      ></div>
+      <button
+        className={`switch-btn ${active === firstTitle ? "active" : ""}`}
+        onClick={() => handleClick(firstTitle)}
+      >
+        {firstTitle}
+      </button>
+      <button
+        className={`switch-btn ${active === secondTitle ? "active" : ""}`}
+        onClick={() => handleClick(secondTitle)}
+      >
+        {secondTitle}
+      </button>
     </div>
-  )
+  );
 }
